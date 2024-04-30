e2c2debd943f1caa2117c6320a780fe3c0caa2a4
Rahul Martim Juliato <rahul.juliato@gmail.com>
2024-04-29 21:18:55 -0300

    feat: dashboard & packages updates

--- init.lua
+++ init.lua
@@ -104,6 +104,38 @@ require("lazy").setup({
 	},
 
 	{
+		"goolord/alpha-nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+
+		config = function()
+			local alpha = require("alpha")
+			local dashboard = require("alpha.themes.startify")
+
+			dashboard.section.header.val = {
+				[[                                                                       ]],
+				[[                                                                       ]],
+				[[                                                                       ]],
+				[[                                                                       ]],
+				[[                                                                     ]],
+				[[       ████ ██████           █████      ██                     ]],
+				[[      ███████████             █████                             ]],
+				[[      █████████ ███████████████████ ███   ███████████   ]],
+				[[     █████████  ███    █████████████ █████ ██████████████   ]],
+				[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+				[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+				[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+				[[                                                                       ]],
+				[[                                                                       ]],
+				[[                                                                       ]],
+			}
+
+			alpha.setup(dashboard.opts)
+		end,
+	},
+
+	{
 		-- Tree explorer
 		"nvim-tree/nvim-tree.lua",
 		dependencies = {
