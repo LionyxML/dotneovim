a3b1286f15500d2a282291e7f8478831211b4fca
Rahul Martim Juliato <rahul.juliato@gmail.com>
2024-06-03 13:29:00 -0300

    feat: gc/gcc are now built in on nvim >=0.10

--- init.lua
+++ init.lua
@@ -449,27 +449,6 @@ require("lazy").setup({
   },
 
   {
-    -- Provides commenting capabilities on line and selection
-    "numToStr/Comment.nvim",
-    opts = {
-      toggler = {
-        ---Line-comment toggle keymap
-        line = "gcc",
-        ---Block-comment toggle keymap
-        block = "gbc",
-      },
-      ---LHS of operator-pending mappings in NORMAL and VISUAL mode
-      opleader = {
-        ---Line-comment keymap
-        line = "gc",
-        ---Block-comment keymap
-        block = "gb",
-      },
-    },
-    lazy = false,
-  },
-
-  {
     -- Fuzzy Finder (files, lsp, etc)
     "nvim-telescope/telescope.nvim",
     branch = "0.1.x",
