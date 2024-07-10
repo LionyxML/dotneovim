53230e455cfca428b9185dd36e62cf679757db6f
Rahul M. Juliato <rahul.juliato@gmail.com>
2024-07-09 22:09:52 -0300

    feat: better filnames on lualine / yanky plugin

--- init.lua
+++ init.lua
@@ -550,6 +550,23 @@ require("lazy").setup({
         section_separators = { left = "", right = "" },
         -- section_separators = { right = "" },
       },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_c = { 'branch', 'diff', 'diagnostics' },
+        lualine_b = { {
+          'filename',
+          path = 4,
+          symbols = {
+            modified = ' ●',
+            alternate_file = '#',
+            directory = '',
+          },
+        } },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+      },
+
     },
   },
   -- }}}
@@ -585,6 +602,37 @@ require("lazy").setup({
           return vim.fn.executable("make") == 1
         end,
       },
+    },
+  },
+  -- }}}
+  -- {{{ Yanky                           Yank history
+  {
+    "gbprod/yanky.nvim",
+    dependencies = {
+      { "kkharji/sqlite.lua" }
+    },
+    opts = {
+      ring = { storage = "sqlite" },
+    },
+    keys = {
+      { "<leader>p", function() require("telescope").extensions.yank_history.yank_history({}) end, desc = "Open Yank History" },
+      { "y",         "<Plug>(YankyYank)",                                                          mode = { "n", "x" },                                desc = "Yank text" },
+      { "p",         "<Plug>(YankyPutAfter)",                                                      mode = { "n", "x" },                                desc = "Put yanked text after cursor" },
+      { "P",         "<Plug>(YankyPutBefore)",                                                     mode = { "n", "x" },                                desc = "Put yanked text before cursor" },
+      { "gp",        "<Plug>(YankyGPutAfter)",                                                     mode = { "n", "x" },                                desc = "Put yanked text after selection" },
+      { "gP",        "<Plug>(YankyGPutBefore)",                                                    mode = { "n", "x" },                                desc = "Put yanked text before selection" },
+      { "<c-p>",     "<Plug>(YankyPreviousEntry)",                                                 desc = "Select previous entry through yank history" },
+      { "<c-n>",     "<Plug>(YankyNextEntry)",                                                     desc = "Select next entry through yank history" },
+      { "]p",        "<Plug>(YankyPutIndentAfterLinewise)",                                        desc = "Put indented after cursor (linewise)" },
+      { "[p",        "<Plug>(YankyPutIndentBeforeLinewise)",                                       desc = "Put indented before cursor (linewise)" },
+      { "]P",        "<Plug>(YankyPutIndentAfterLinewise)",                                        desc = "Put indented after cursor (linewise)" },
+      { "[P",        "<Plug>(YankyPutIndentBeforeLinewise)",                                       desc = "Put indented before cursor (linewise)" },
+      { ">p",        "<Plug>(YankyPutIndentAfterShiftRight)",                                      desc = "Put and indent right" },
+      { "<p",        "<Plug>(YankyPutIndentAfterShiftLeft)",                                       desc = "Put and indent left" },
+      { ">P",        "<Plug>(YankyPutIndentBeforeShiftRight)",                                     desc = "Put before and indent right" },
+      { "<P",        "<Plug>(YankyPutIndentBeforeShiftLeft)",                                      desc = "Put before and indent left" },
+      { "=p",        "<Plug>(YankyPutAfterFilter)",                                                desc = "Put after applying a filter" },
+      { "=P",        "<Plug>(YankyPutBeforeFilter)",                                               desc = "Put before applying a filter" },
     },
   },
   -- }}}
@@ -1079,7 +1127,7 @@ require("which-key").register({
     b = { ":Git blame<CR>", "Blame File" },
     n = { ":Neogit<cr>", "Neogit" },
   },
-  ["<leader>h"] = { name = "More git", _ = "which_key_ignore" },
+  ["<{leader>h"] = { name = "More git", _ = "which_key_ignore" },
   ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
   ["<leader>s"] = {
     name = "[S]earch",
