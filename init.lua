401523f6ae2e2799cc7b291ddf9a1a938249ab30
Rahul Martim Juliato <rahul.juliato@gmail.com>
2024-08-11 21:54:17 -0300

    feat(auto-session): makes it on by default

--- init.lua
+++ init.lua
@@ -755,7 +755,8 @@ require("lazy").setup({
 			local auto_session = require("auto-session")
 
 			auto_session.setup({
-				auto_restore_enabled = false,
+				auto_session_enabled = true,
+				auto_restore_enabled = true,
 				auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
 			})
 
