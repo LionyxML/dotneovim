d75ceae1e7f7eddc6c145fab48408d3494eb1901
Rahul Martim Juliato <rahul.juliato@gmail.com>
2024-09-01 23:44:35 -0300

    feat(legendary): a command palette

--- init.lua
+++ init.lua
@@ -67,6 +67,17 @@ end
 vim.opt.rtp:prepend(lazypath)
 --- }}}
 require("lazy").setup({
+	-- {{{ Legendary
+	{
+		"mrjones2014/legendary.nvim",
+		-- since legendary.nvim handles all your keymaps/commands,
+		-- its recommended to load legendary.nvim before other plugins
+		priority = 10000,
+		lazy = false,
+		-- sqlite is only needed if you want to use frecency sorting
+		-- dependencies = { 'kkharji/sqlite.lua' }
+	},
+	-- }}}
 	-- {{{ Vim-Fugitive                    A git wrapper for vim
 	{
 		"tpope/vim-fugitive",
