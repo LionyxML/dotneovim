b0a479a247f37fdd0985b4cfe4a09fdf370e171b
Rahul Martim Juliato <rahul.juliato@gmail.com>
2024-09-09 09:14:33 -0300

    feat(ts-comments): enhances built-in comments

--- init.lua
+++ init.lua
@@ -695,6 +695,14 @@ require("lazy").setup({
 		},
 	},
 	-- }}}
+	-- {{{ TS-Comments                     Adds tree sitter context to nvim built-in commenter
+	{
+		"folke/ts-comments.nvim",
+		opts = {},
+		event = "VeryLazy",
+		enabled = vim.fn.has("nvim-0.10.0") == 1,
+	},
+	-- }}}
 	-- {{{ Aerial                          The tree viewer for symbols
 	{
 		"stevearc/aerial.nvim",
