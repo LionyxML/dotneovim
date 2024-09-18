85789a49ffd7952dd132cdb37405ab779f3d5984
Rahul Martim Juliato <rahul.juliato@gmail.com>
2024-09-18 17:08:16 -0300

    fix(conform): removed conflicting config

--- init.lua
+++ init.lua
@@ -866,7 +866,7 @@ require("lazy").setup({
 				format_on_save = {
 					lsp_fallback = true,
 					async = false,
-					timeout_ms = 5000000,
+					timeout_ms = 5000,
 				},
 			})
 
@@ -877,16 +877,6 @@ require("lazy").setup({
 					timeout_ms = 5000,
 				})
 			end, { desc = "Format file or range (in visual mode)" })
-
-			require("conform").setup({
-				format_on_save = function(bufnr)
-					-- Disable with a global or buffer-local variable
-					if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-						return
-					end
-					return { timeout_ms = 500, lsp_format = "fallback" }
-				end,
-			})
 
 			vim.api.nvim_create_user_command("FormatDisable", function(args)
 				if args.bang then
