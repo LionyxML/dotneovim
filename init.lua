4611fbc1e13b071f26000a6e5817fa35d27db5e0
Rahul M. Juliato <rahul.juliato@gmail.com>
2024-12-02 20:03:44 -0300

    feat(update-plugins)

--- init.lua
+++ init.lua
@@ -155,24 +155,6 @@ require("lazy").setup({
 		end,
 	},
 	-- }}}
-	-- {{{ Nvim-Tmux-Navigator             Integration with tmux
-	{
-		"alexghergh/nvim-tmux-navigation",
-		config = function()
-			require("nvim-tmux-navigation").setup({
-				disable_when_zoomed = true, -- defaults to false
-				keybindings = {
-					left = "<C-h>",
-					down = "<C-j>",
-					up = "<C-k>",
-					right = "<C-l>",
-					-- last_active = "<C-\\>",
-					-- next = "<C-Space>",
-				},
-			})
-		end,
-	},
-	-- }}}
 	-- {{{ Vim-Sleuth                      Detect tabstop and shiftwidth automatically
 	{
 		"tpope/vim-sleuth",
