d0604d4e6990136da262ee6641eac748de356857
Rahul Martim Juliato <rahul.juliato@gmail.com>
2025-02-06 20:20:20 -0300

    feat: <leader> co - organize imports

--- init.lua
+++ init.lua
@@ -267,6 +267,14 @@ require("lazy").setup({
 					-- or a suggestion from your LSP for this to activate.
 					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
+					map("<leader>co", function()
+						vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })
+					end, "[C]ode [O]rganize imports")
+
+					-- vim.keymap.set("n", "<leader>co", function()
+					-- 	vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })
+					-- end, { noremap = true, silent = true, expr = false, desc = "LSP: [C]ode [O]rganize imports" })
+
 					-- Opens a popup that displays documentation about the word under your cursor
 					--  See `:help K` for why this keymap.
 					map("K", vim.lsp.buf.hover, "Hover Documentation")
