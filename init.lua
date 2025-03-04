f368cc91ffc5b09fc4df404f3a22d5cac82e1eb8
Rahul Martim Juliato <rahul.juliato@gmail.com>
2025-03-04 20:31:40 -0300

    feat: hint diagnostic icon to match snaks explorer

--- init.lua
+++ init.lua
@@ -1647,7 +1647,7 @@ vim.diagnostic.config({
 		text = {
 			[vim.diagnostic.severity.ERROR] = "󰅚 ",
 			[vim.diagnostic.severity.WARN] = "󰀪 ",
-			[vim.diagnostic.severity.HINT] = "󰌶 ",
+			[vim.diagnostic.severity.HINT] = " ",
 			[vim.diagnostic.severity.INFO] = " ",
 		},
 	},
