54195dfa6dce54681399f7bde0235ac4a548eb10
Rahul Martim Juliato <rahul.juliato@gmail.com>
2025-09-20 10:57:57 -0300

    fix: prefer the new vim.lsp.config directive

--- init.lua
+++ init.lua
@@ -532,18 +532,19 @@ require("lazy").setup({
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
-			require("mason-lspconfig").setup({
-				handlers = {
-					function(server_name)
-						local server = servers[server_name] or {}
-						-- This handles overriding only values explicitly passed
-						-- by the server configuration above. Useful when disabling
-						-- certain features of an LSP (for example, turning off formatting for tsserver)
-						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-						require("lspconfig")[server_name].setup(server)
-					end,
-				},
-			})
+			-- NOTE: kepp an eye on:
+			-- https://github.com/nvim-lua/kickstart.nvim/pull/1663
+			-- https://github.com/nvim-lua/kickstart.nvim/pull/1590
+
+			-- The following loop will configure each server with the capabilities we defined above.
+			-- This will ensure that all servers have the same base configuration, but also
+			-- allow for server-specific overrides.
+			for server_name, server_config in pairs(servers) do
+				server_config.capabilities =
+					vim.tbl_deep_extend("force", {}, capabilities, server_config.capabilities or {})
+				vim.lsp.config(server_name, server_config)
+				vim.lsp.enable(server_name)
+			end
 		end,
 	},
 	-- }}}
