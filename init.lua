e8d0f8194eebfb9fdc330b30813191769044b7a5
Rahul Martim Juliato <rahul.juliato@gmail.com>
2026-02-13 10:41:00 -0300

    chore: later and docs refactors

--- init.lua
+++ init.lua
@@ -1,12 +1,13 @@
---    LionyxML Config for:
---                                              
---       ████ ██████           █████      ██
---      ███████████             █████ 
---      █████████ ███████████████████ ███   ███████████
---     █████████  ███    █████████████ █████ ██████████████
---    █████████ ██████████ █████████ █████ █████ ████ █████
---  ███████████ ███    ███ █████████ █████ █████ ████ █████
--- ██████  █████████████████████ ████ █████ █████ ████ ██████
+-- Lionyx Config for:
+--
+--  _        _______  _______          _________ _______
+-- ( (    /|(  ____ \(  ___  )|\     /|\__   __/(       )
+-- |  \  ( || (    \/| (   ) || )   ( |   ) (   | () () |
+-- |   \ | || (__    | |   | || |   | |   | |   | || || |
+-- | (\ \) ||  __)   | |   | |( (   ) )   | |   | |(_)| |
+-- | | \   || (      | |   | | \ \_/ /    | |   | |   | |
+-- | )  \  || (____/\| (___) |  \   /  ___) (___| )   ( |
+-- |/    )_)(_______/(_______)   \_/   \_______/|/     \|
 --
 --
 -- {{{ About this config
@@ -19,29 +20,7 @@
 -- Author:        Rahul Martim Juliato <rahul.juliato@gmail.com>
 -- Created:       2023-10-31
 -- Last Modified: today? yestarday? tomorrow? (check github.com/lionyxml)
---
--- =============================================================================
---                                  NOTES
--- =============================================================================
---
--- This Neovim configuration started from kickstart.nvim
--- (https://github.com/mhinz/vim-startify) and has been extended with
--- additional customizations.
---
--- The configuration is provided under the terms of the GNU General Public
--- License, version 2 (GPL-2.0). You are free to copy, modify, and distribute
--- this configuration, provided you include this license notice.
---
--- For the full text of the GPL-2.0 license, please visit:
--- https://www.gnu.org/licenses/gpl-2.0.html
---
--- This configuration uses the built-in vim.pack package manager (Neovim 0.12+).
--- All plugins load eagerly at startup (no lazy loading).
---
--- Make sure to check the documentation of plugins for additional settings.
---
--- =============================================================================
---                                HAPPY VIMMING!
+-- Requires:      neovim 0.12 or higher
 -- =============================================================================
 -- }}}
 -- {{{ Load timer
@@ -177,11 +156,6 @@ vim.api.nvim_create_autocmd("User", {
 -- }}}
 -- {{{ Catppuccin                      UI - The Only and One Theme :)
 require("catppuccin").setup({
-	-- color_overrides = {
-	--   mocha = {
-	--     base = "#000000",
-	--   },
-	-- },
 	flavour = "mocha",
 	transparent_background = true,
 	float = {
@@ -214,149 +188,150 @@ require("catppuccin").setup({
 vim.cmd.colorscheme("catppuccin")
 -- }}}
 -- {{{ LSPConfig                       CODE - LSP Configurations and plugins
+later(function()
+	-- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
+	-- used for completion, annotations and signatures of Neovim apis
+	require("neodev").setup({})
 
--- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
--- used for completion, annotations and signatures of Neovim apis
-require("neodev").setup({})
+	--  This function gets run when an LSP attaches to a particular buffer.
+	--    That is to say, every time a new file is opened that is associated with
+	--    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
+	--    function will be executed to configure the current buffer
+	vim.api.nvim_create_autocmd("LspAttach", {
+		group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+		callback = function(event)
+			local map = function(keys, func, desc)
+				vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+			end
 
---  This function gets run when an LSP attaches to a particular buffer.
---    That is to say, every time a new file is opened that is associated with
---    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
---    function will be executed to configure the current buffer
-vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
-	callback = function(event)
-		local map = function(keys, func, desc)
-			vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-		end
+			map("<leader>ln", vim.lsp.buf.rename, "re[n]ame")
 
-		map("<leader>ln", vim.lsp.buf.rename, "re[n]ame")
+			map("<leader>la", vim.lsp.buf.code_action, "code [a]ction")
 
-		map("<leader>la", vim.lsp.buf.code_action, "code [a]ction")
+			map("<leader>lo", function()
+				vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })
+			end, "[o]rganize imports")
 
-		map("<leader>lo", function()
-			vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })
-		end, "[o]rganize imports")
+			map("K", vim.lsp.buf.hover, "Hover Documentation")
 
-		map("K", vim.lsp.buf.hover, "Hover Documentation")
+			local client = vim.lsp.get_client_by_id(event.data.client_id)
+			if client and client.server_capabilities.documentHighlightProvider then
+				local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+				vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+					buffer = event.buf,
+					group = highlight_augroup,
+					callback = vim.lsp.buf.document_highlight,
+				})
 
-		local client = vim.lsp.get_client_by_id(event.data.client_id)
-		if client and client.server_capabilities.documentHighlightProvider then
-			local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
-			vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-				buffer = event.buf,
-				group = highlight_augroup,
-				callback = vim.lsp.buf.document_highlight,
-			})
+				vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+					buffer = event.buf,
+					group = highlight_augroup,
+					callback = vim.lsp.buf.clear_references,
+				})
 
-			vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-				buffer = event.buf,
-				group = highlight_augroup,
-				callback = vim.lsp.buf.clear_references,
-			})
+				vim.api.nvim_create_autocmd("LspDetach", {
+					group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+					callback = function(event2)
+						vim.lsp.buf.clear_references()
+						vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+					end,
+				})
+			end
 
-			vim.api.nvim_create_autocmd("LspDetach", {
-				group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
-				callback = function(event2)
-					vim.lsp.buf.clear_references()
-					vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
-				end,
-			})
-		end
+			if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+				map("<leader>th", function()
+					vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+				end, "Toggle inlay [h]ints")
+			end
+		end,
+	})
 
-		if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-			map("<leader>th", function()
-				vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-			end, "Toggle inlay [h]ints")
-		end
-	end,
-})
+	local capabilities = vim.lsp.protocol.make_client_capabilities()
+	capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-
-local servers = {
-	-- ruff = {},
-	pyright = {},
-	gopls = {},
-	prismals = {},
-	-- htmx = {},
-	bashls = {},
-	rust_analyzer = {},
-	-- rubocop = {},
-	ruby_lsp = {},
-	jsonls = {},
-	cssls = {},
-	somesass_ls = { "scss", "sass" },
-	biome = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-	ts_ls = {
-		filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-		-- TODO: this is not working yet for typescript
-		settings = {
-			typescript = {
-				inlayHints = {
-					includeInlayParameterNameHints = "all",
-					includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-					includeInlayFunctionParameterTypeHints = true,
-					includeInlayVariableTypeHints = true,
-					includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-					includeInlayPropertyDeclarationTypeHints = true,
-					includeInlayFunctionLikeReturnTypeHints = true,
-					includeInlayEnumMemberValueHints = true,
+	local servers = {
+		-- ruff = {},
+		pyright = {},
+		gopls = {},
+		prismals = {},
+		-- htmx = {},
+		bashls = {},
+		rust_analyzer = {},
+		-- rubocop = {},
+		ruby_lsp = {},
+		jsonls = {},
+		cssls = {},
+		somesass_ls = { "scss", "sass" },
+		biome = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+		ts_ls = {
+			filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+			-- TODO: this is not working yet for typescript
+			settings = {
+				typescript = {
+					inlayHints = {
+						includeInlayParameterNameHints = "all",
+						includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+						includeInlayFunctionParameterTypeHints = true,
+						includeInlayVariableTypeHints = true,
+						includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+						includeInlayPropertyDeclarationTypeHints = true,
+						includeInlayFunctionLikeReturnTypeHints = true,
+						includeInlayEnumMemberValueHints = true,
+					},
 				},
 			},
 		},
-	},
-	eslint = { filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" } },
-	tailwindcss = {},
-	html = {},
-	-- html = { filetypes = { 'html', 'twig', 'hbs'} },
-	lua_ls = {
-		Lua = {
-			workspace = { checkThirdParty = false },
-			telemetry = { enable = false },
-			hint = { enable = true },
-			diagnostics = {
-				enable = true,
-				globals = {
-					"vim",
-					"describe",
-					"it",
-					"before_each",
-					"after_each",
-					"packer_plugins",
-					"MiniTest",
+		eslint = { filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" } },
+		tailwindcss = {},
+		html = {},
+		-- html = { filetypes = { 'html', 'twig', 'hbs'} },
+		lua_ls = {
+			Lua = {
+				workspace = { checkThirdParty = false },
+				telemetry = { enable = false },
+				hint = { enable = true },
+				diagnostics = {
+					enable = true,
+					globals = {
+						"vim",
+						"describe",
+						"it",
+						"before_each",
+						"after_each",
+						"packer_plugins",
+						"MiniTest",
+					},
+					disable = { "missing-fields", "lowercase-global" },
 				},
-				disable = { "missing-fields", "lowercase-global" },
 			},
 		},
-	},
-}
+	}
 
-require("mason").setup({
-	ui = {
-		border = vim.o.winborder,
-	},
-})
+	require("mason").setup({
+		ui = {
+			border = vim.o.winborder,
+		},
+	})
 
-local ensure_installed = vim.tbl_keys(servers or {})
-vim.list_extend(ensure_installed, {
-	"stylua", -- Used to format Lua code
-})
-require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+	local ensure_installed = vim.tbl_keys(servers or {})
+	vim.list_extend(ensure_installed, {
+		"stylua", -- Used to format Lua code
+	})
+	require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
--- NOTE: kepp an eye on:
--- https://github.com/nvim-lua/kickstart.nvim/pull/1663
--- https://github.com/nvim-lua/kickstart.nvim/pull/1590
+	-- NOTE: kepp an eye on:
+	-- https://github.com/nvim-lua/kickstart.nvim/pull/1663
+	-- https://github.com/nvim-lua/kickstart.nvim/pull/1590
 
--- The following loop will configure each server with the capabilities we defined above.
--- This will ensure that all servers have the same base configuration, but also
--- allow for server-specific overrides.
-for server_name, server_config in pairs(servers) do
-	server_config.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server_config.capabilities or {})
-	vim.lsp.config(server_name, server_config)
-	vim.lsp.enable(server_name)
-end
+	-- The following loop will configure each server with the capabilities we defined above.
+	-- This will ensure that all servers have the same base configuration, but also
+	-- allow for server-specific overrides.
+	for server_name, server_config in pairs(servers) do
+		server_config.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server_config.capabilities or {})
+		vim.lsp.config(server_name, server_config)
+		vim.lsp.enable(server_name)
+	end
+end)
 
 -- }}}
 -- {{{ Nvim-Cmp                        EDIT - Autocompletion
@@ -645,7 +620,6 @@ later(function()
 end)
 
 -- Diff (c)hunk naviation and git gutter
--- later(function()
 require("mini.diff").setup({
 	view = {
 		signs = {
@@ -660,7 +634,6 @@ require("mini.diff").setup({
 		goto_next = "]c",
 	},
 })
--- end)
 
 -- }}}
 -- {{{ UndoTree                        EDIT - Perfect pitch Undoing
@@ -737,11 +710,11 @@ end)
 -- vim-sleuth configures itself automatically, no setup needed
 -- }}}
 -- {{{ Nvim-Treesitter-Context         TXT - Adds tree sitter context to the buffer
--- later(function()
-require("treesitter-context").setup({
-	enable = true, -- Defaults to disabled, use <leader>tc to toggle Context
-})
--- end)
+later(function()
+	require("treesitter-context").setup({
+		enable = true, -- Defaults to disabled, use <leader>tc to toggle Context
+	})
+end)
 -- }}}
 -- {{{ Aerial                          TXT - The tree viewer for symbols
 later(function()
@@ -751,7 +724,6 @@ later(function()
 end)
 -- }}}
 -- {{{ Rainbow Delimiters              TXT - Colorize parentheses, brackets, etc
--- later(function()
 require("rainbow-delimiters.setup").setup({
 	query = {
 		[""] = "rainbow-delimiters",
@@ -760,7 +732,6 @@ require("rainbow-delimiters.setup").setup({
 		tsx = "rainbow-parens",
 	},
 })
--- end)
 -- }}}
 -- {{{ TreeSitter                      TXT - Highlight, edit and navigate code
 --
