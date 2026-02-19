c3e976f0107690e88e8b66be3381e5ce8c1fc567
Rahul Martim Juliato <rahul.juliato@gmail.com>
2026-02-19 16:40:17 -0300

    feat: floating lazygit feature

--- init.lua
+++ init.lua
@@ -1325,5 +1325,50 @@ later(function()
 	})
 end)
 --}}}
+--{{{ MY - LAZYGIT
+later(function()
+	function LazyGitFloating()
+		-- Calculate floating window size
+		local width = math.floor(vim.o.columns * 0.8)
+		local height = math.floor(vim.o.lines * 0.8)
+		local row = math.floor((vim.o.lines - height) / 2)
+		local col = math.floor((vim.o.columns - width) / 2)
+
+		-- Create a scratch buffer
+		local buf = vim.api.nvim_create_buf(false, true)
+
+		-- Open floating window
+		vim.api.nvim_open_win(buf, true, {
+			relative = "editor",
+			width = width,
+			height = height,
+			row = row,
+			col = col,
+			style = "minimal",
+		})
+
+		-- Start lazygit in terminal
+		vim.fn.termopen("lazygit", {
+			on_exit = function()
+				-- Close buffer and window when process exits
+				if vim.api.nvim_buf_is_valid(buf) then
+					vim.api.nvim_buf_delete(buf, { force = true })
+				end
+			end,
+		})
+
+		-- Enter insert mode automatically
+		vim.cmd("startinsert")
+	end
+
+	-- Create a user command to call it
+	vim.api.nvim_create_user_command("LazyGit", LazyGitFloating, {})
+
+	vim.keymap.set("n", "<leader>L", "<Cmd>LazyGit<CR>", {
+		desc = "[L]azyGit",
+	})
+end)
+
+--}}}
 
 -- vim: ts=2 sts=2 sw=2 et fileencoding=utf-8:foldmethod=marker
