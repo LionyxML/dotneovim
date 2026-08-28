12c7cd831851fb043e37c21cc2f08e9320833631
Rahul Martim Juliato <rahul.juliato@gmail.com>
2026-08-27 22:30:46 -0300

    fix: some new ui2 api changes

--- init.lua
+++ init.lua
@@ -1164,15 +1164,15 @@ require("vim._core.ui2").enable({
 			wmsg = "msg",
 			typed_cmd = "cmd",
 		},
-		cmd = {
-			height = 0.5,
-		},
 		dialog = {
 			height = 0.5,
 		},
 		msg = {
 			height = 0.3,
+		},
+		messagesopt = {
 			timeout = 5000,
+			maxheight = 0.5,
 		},
 		pager = {
 			height = 0.5,
